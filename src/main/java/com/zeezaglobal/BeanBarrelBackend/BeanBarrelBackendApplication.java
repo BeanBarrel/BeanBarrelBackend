91a30e03d85ac1042cfbb26b7109e1517d96a449
zeezaglobal <96932409+zeezaglobal@users.noreply.github.com>
2025-07-16 00:11:32 -0500

    init

---
+++ src/main/java/com/zeezaglobal/BeanBarrelBackend/BeanBarrelBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zeezaglobal.BeanBarrelBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanBarrelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanBarrelBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeezaglobal/BeanBarrelBackend/BeanBarrelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeezaglobal.BeanBarrelBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanBarrelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
